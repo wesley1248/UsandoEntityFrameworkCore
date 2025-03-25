32bf9a160b7a22b28237654cd03084f16c2f5578
wesley1248 <wesleytms.es@gmail.com>
2025-03-25 08:02:19 -0300

    Home v.0001

---
+++ Controllers/Estudante.cs
@@ -0,0 +1,20 @@
+﻿using AulaEntity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AulaEntity.Controllers
+{
+    public class Estudante : Controller
+    {
+
+        #region "PROPRIEDADES"
+
+        private readonly AppDbContext _context;
+
+        #endregion
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
