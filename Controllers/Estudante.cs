9c5771280192aac9b864b090ac0dbd18330fb97d
wesley1248 <wesleytms.es@gmail.com>
2025-03-27 19:50:12 -0300

    Front v.02

--- Controllers/Estudante.cs
+++ Controllers/Estudante.cs
@@ -38,7 +38,61 @@ namespace AulaEntity.Controllers
             catch (Exception ex)
             {
                 string mensagemErro = ex.InnerException?.Message ?? ex.Message;
-                return Json(new { status = 0, message = mensagemErro });
+                return Json(new { status = 3, message = mensagemErro });
+            }
+        }
+
+        public async Task<JsonResult> EditarEstudante()
+        {
+            int linhasAfetadas = 0;
+
+            try
+            {
+                var primeiroId = await _context.Estudantes
+                    .Where(e => e.Nome != "AAA")
+                    .Select(e => e.Id)
+                    .FirstOrDefaultAsync();
+
+                if (primeiroId != Guid.Empty)
+                {
+                    linhasAfetadas = await _context.Estudantes
+                        .Where(e => e.Id == primeiroId)
+                        .ExecuteUpdateAsync(s => s.SetProperty(e => e.Nome, "AAA"));
+                }
+
+                return Json(new { status = linhasAfetadas });
+            }
+            catch (Exception ex)
+            {
+                string mensagemErro = ex.InnerException?.Message ?? ex.Message;
+                return Json(new { status = 3, message = mensagemErro });
+            }
+        }
+
+        public async Task<JsonResult> ExcluirEstudante()
+        {
+            int linhasAfetadas = 0;
+
+            try
+            {
+                var primeiroId = await _context.Estudantes
+                    .Where(e => e.Nome != "AAA")
+                    .Select(e => e.Id)
+                    .FirstOrDefaultAsync();
+
+                if (primeiroId != Guid.Empty)
+                {
+                    linhasAfetadas = await _context.Estudantes
+                        .Where(e => e.Id == primeiroId)
+                        .ExecuteDeleteAsync();
+                }
+
+                return Json(new { status = linhasAfetadas });
+            }
+            catch (Exception ex)
+            {
+                string mensagemErro = ex.InnerException?.Message ?? ex.Message;
+                return Json(new { status = 3, message = mensagemErro });
             }
         }
     }
